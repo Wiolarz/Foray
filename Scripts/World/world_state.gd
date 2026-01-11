20d060e69dfff2e32fc74275fd1c20f7467f4db1
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2026-01-11 20:49:44 +0100

    world small bugfix

--- Scripts/World/world_state.gd
+++ Scripts/World/world_state.gd
@@ -670,7 +670,7 @@ func change_army_position(army : Army, target_coord : Vector2i) -> void:
 		var number_of_units_to_be_left : int = army.units_data.size() - army.max_army_size
 
 		for unit_over_limit_idx in range(number_of_units_to_be_left):
-			source_hex.place.garrison_reserve.units_data.append(army.units_data[-unit_over_limit_idx - 1])
+			source_hex.place.garrison_reserve.units_data.append(army.units_data[-1])
 			army.units_data.pop_back()
 
 		WM.world_ui.refresh_army_panel()
